c1f78de708d92e3b8b97ce44ebd0569391e9e91d
kylelainez <kyleadrianlainez@gmail.com>
2020-12-11 12:47:11 -0800

    Create Guitar Function

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home")
+    path('', views.home, name="home"),
+    path('guitars', views.index, name="index"),
+    path('guitars/create/', views.CreateGuitar.as_view(), name="create"),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Guitar
 
 # Create your views here.
+class CreateGuitar(CreateView):
+    model = Guitar
+    fields = '__all__'
 
 def home(request):
+    return render(request, 'home.html')
+
+def index(request):
     return render(request, 'home.html')
